4fdad5521b50056c6f3191eb7ccd5fd2d2c1707a
Shadowblitz16 <Shadowblitz16@gmail.com>
2022-12-14 19:12:32 -0800

    Building an OS - 3 - The Fat file system

---
+++ src/kernel/main.asm
@@ -0,0 +1,40 @@
+org 0x0
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+start:
+	mov si, msg_hello
+	call puts
+
+.halt:
+	cli
+	hlt
+
+; prints a string to the screen
+; Params:
+;   -ds:si points to string
+puts:
+    ; save registers we will modify
+    push si
+    push ax
+	push bx
+
+.loop:
+	lodsb				; loads next character in al
+	or al, al			; verify if next charcter is null?
+	jz .done
+
+	mov ah, 0x0E		; call bois interupts
+	mov bh, 0			; set page number to 0
+	int 0x10
+
+	jmp .loop
+
+.done:
+    pop bx
+	pop ax
+	pop si
+	ret
+
+msg_hello: db 'Hello world!', ENDL, 0
